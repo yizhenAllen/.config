7be06b351ca9fd2570a8e3646a101bcf0b11f541
yizhenAllen <614153534@qq.com>
2023-08-20 09:04:31 +0800

    Change the color of float window and git sign

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -31,6 +31,14 @@ return {
       -- set the cursor blinking
       vim.cmd(
         "set guicursor=n-v-c:block,i-ci-ve:ver25,r-cr:hor20,o:hor50,a:blinkwait700-blinkoff400-blinkon250-Cursor/lCursor,sm:block-blinkwait175-blinkoff150-blinkon175")
+      -- set float window and git signs color
+      vim.cmd("hi NormalFloat guibg=#32302F")
+      vim.cmd("hi FloatBorder guibg=#32302F")
+      vim.cmd("hi SignColumn guibg=#32302F")
+      vim.cmd("hi GitGutterAdd guibg=#32302F ctermfg=142 ctermbg=237 guifg=#b8bb26")
+      vim.cmd("hi GitGutterDelete ctermfg=167 ctermbg=237 guifg=#fb4934 guibg=#32302F")
+      vim.cmd("hi GitGutterChange ctermfg=108 ctermbg=237 guifg=#8ec07c guibg=#32302F")
+      vim.cmd("hi GitGutterChangeDelete ctermfg=108 ctermbg=237 guifg=#8ec07c guibg=#32302F")
     end
   }
 
